6ed2a56b6aaf1c943b3c0f82df4965e97f216433
gaole <gaole@umich.edu>
2017-05-13 21:22:32 -0400

    add search engine

--- static/simulation.js
+++ static/simulation.js
@@ -13,6 +13,8 @@ var raycaster;
 var precolor;
 var theaxis;
 
+
+
 var curTxt=document.createElement('div');
 document.body.appendChild(curTxt);
 
@@ -34,7 +36,7 @@ $.getJSON('/static/emoji_json_withname.json',function(data){
 
    renderer.setClearColor( 0xffffff );
   var stats = new Stats();
-  container.appendChild( stats.dom );
+  //container.appendChild( stats.dom );
   //  var newbutton = document.createElement('div');
   //  document.body.appendChild(newbutton);
   // newbutton.innerHTML="sdkusdhgfkusbfkusbfsbfdsbf,sdbfsfjksbfhlaijl";
@@ -44,7 +46,6 @@ $.getJSON('/static/emoji_json_withname.json',function(data){
    render();
    window.addEventListener('resize' , onWindowResize , false);
 
-   console.log(document.getElementById("container").offsetWidth);
 
 
    function init(){
@@ -62,7 +63,7 @@ $.getJSON('/static/emoji_json_withname.json',function(data){
 
     // add the control
    controls = new THREE.TrackballControls( camera );
-   controls.rotateSpeed = 1.5;
+   controls.rotateSpeed = 2;
    controls.zoomSpeed = 1.2;
    controls.panSpeed = 0.8;
    controls.noZoom = false;
@@ -109,7 +110,7 @@ $.getJSON('/static/emoji_json_withname.json',function(data){
       xbar+=parseFloat(data[i].x);
       ybar+=parseFloat(data[i].y);
       zbar+=parseFloat(data[i].z);
-      console.log(data[i].x + " " + i+ " "+xbar);
+  
       
     }
   
@@ -118,10 +119,9 @@ $.getJSON('/static/emoji_json_withname.json',function(data){
     ybar/=datanum;
     zbar/=datanum;
 
-    console.log(xbar+ " "+ ybar+" "+zbar); 
     
 
-    for (let i = 1; i <= datanum ; i++){
+    for (var i = 1; i <= datanum ; i++){
       // var object;
       // if (i == 193 || i == 180){
       //   object = new THREE.Mesh( sphereGeo,  new THREE.MeshBasicMaterial({ color: 0xff0030, opacity: 0.5, transparent: true} ) )
@@ -144,8 +144,19 @@ $.getJSON('/static/emoji_json_withname.json',function(data){
 
       // try 2d image;
       if (data[i].order <=1028 && data[i].order>=1024) continue;
+
       str = "/static/imgs/"+ data[i].order +".png"
-      var map = new THREE.TextureLoader().load( str );
+      // var map
+      // var textureLoader = new THREE.TextureLoader();
+      // textureLoader.load(str);
+
+      //   // Add the event listener
+      var material;
+      var map = new THREE.TextureLoader().load( str , function(tex){
+         // TweenMax.to(sprite.material, 1, { opacity: 1 });
+         //var material = new THREE.SpriteMaterial( { map: tex, color: 0xffffff, fog: true } 
+
+      });
       var material = new THREE.SpriteMaterial( { map: map, color: 0xffffff, fog: true } );
       var sprite = new THREE.Sprite( material );
 
@@ -162,10 +173,10 @@ $.getJSON('/static/emoji_json_withname.json',function(data){
       sprite.position.set(position.x, position.y, position.z);
 
       sprite.num = i;
-
+      sprite.material.opacity = 0;
+      TweenMax.to(sprite.material, 1, { opacity: 1 });
+      objects.push(sprite);
       scene.add( sprite );
-
-
     }
 
     //conbineMesh = new THREE.Mesh(conbineGeo, material);
@@ -241,7 +252,95 @@ $.getJSON('/static/emoji_json_withname.json',function(data){
     camera.aspect = window.innerWidth / window.innerHeight;
     renderer.setSize( window.innerWidth, window.innerHeight );
    }
+   $("#submitb").click(function(){
 
-   
+     //console.log("get")
+    // console.log($(":input").val)
+      let i = 0;
+      for (i = 1; i <= datanum; i++){
+        if (data[i].order <=1028 && data[i].order>=1024) continue;
+        console.log(data[i].name, i, data[i].order);
+        if (data[i].name.indexOf($(':input').val()) != -1){
+          var tmp = i-1
+          if (data[i].order >= 1029) tmp = tmp-5;
+          TweenMax.to(objects[tmp].material, 1, { opacity: 1 });
+        }
+        else{
+          var tmp = i-1
+          if (data[i].order >= 1029) tmp = tmp-5;
+          TweenMax.to(objects[tmp].material, 1, { opacity: 0.2 });
+        }
+      }
+      console.log($(':input').val())
+      $(':input').val("");
+   })
+    $("#back").click(function(){
+
+     console.log("get")
+    // console.log($(":input").val)
+      let i = 0;
+      for (i = 1; i <= datanum; i++){
+        if (data[i].order <=1028 && data[i].order>=1024) continue;
+          if (i >= 1029) {
+            objects[i-6].material.color.set(0xffffff);
+          }
+          else{
+            objects[i-1].material.color.set(0xffffff);
+          }
+        }
+      $(':input').val("");
+    })
+
+
+
+
+
+   function trackOriginalOpacities(mesh) {
+    
+    var opacities = [], 
+        materials = mesh.material.materials ? mesh.material.materials : [mesh.material];
+    for (var i = 0; i < materials.length; i++) {        
+         materials[i].transparent = true;
+         opacities.push(materials[i].opacity);
+    }
+    mesh.userData.originalOpacities = opacities;
+  }
+
+    function fadeMesh(mesh, direction, options) {
+        options = options || {};
+        // set and check 
+        var current = { percentage : direction == "in" ? 1 : 0 },
+        // this check is used to work with normal and multi materials.
+        mats = mesh.material.materials ? 
+                 mesh.material.materials : [mesh.material],
+     
+         originals = mesh.userData.originalOpacities,
+         easing = options.easing || TWEEN.Easing.Linear.None,
+         duration = options.duration || 2000;
+        // check to make sure originals exist
+        if( !originals ) {
+             console.error("Fade error: originalOpacities not defined, use trackOriginalOpacities");
+              return;
+        }
+        // tween opacity back to originals
+        var tweenOpacity = new TWEEN.Tween(current)
+            .to({ percentage: direction == "in" ? 0 : 1 }, duration)
+            .easing(easing)
+            .onUpdate(function() {
+                 for (var i = 0; i < mats.length; i++) {
+                    mats[i].opacity = originals[i] * current.percentage;
+                 }
+             })
+             .onComplete(function(){
+                  if(options.callback){
+                       options.callback();
+                  }
+             });
+        tweenOpacity.start();
+        return tweenOpacity;
+    }
 
 })
+window.fadeIn = function(obj) {
+    $(obj).fadeIn(1000);
+}
